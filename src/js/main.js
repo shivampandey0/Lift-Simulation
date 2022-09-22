b4490b53935fdac16186e85e4a286aa49063e85b
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-09-22 21:05:33 +0530

    feat: find closest lift to the floor

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ function buildSimulation(floors, lifts) {
   for (let i = 0; i < lifts; i++) {
     const lift = document.createElement('div');
     lift.setAttribute('class', 'lift');
-    lift.setAttribute('data-floor', `0`);
+    lift.setAttribute('data-floor', `1`);
     lift.setAttribute('data-lift', `${i + 1}`);
     liftsGroup.append(lift);
   }
@@ -33,7 +33,7 @@ function buildSimulation(floors, lifts) {
     const liftButtons = document.createElement('div');
     liftButtons.setAttribute('class', 'lift-buttons');
     const callBtn = document.createElement('button');
-    callBtn.setAttribute('class', `floor-call`);
+    callBtn.setAttribute('class', `floor-call-btn`);
     callBtn.setAttribute('data-floor', `${i + 1}`);
     callBtn.textContent = 'Call';
     const floorTitle = document.createElement('h3');
@@ -50,35 +50,55 @@ function buildSimulation(floors, lifts) {
 
   floorsGroup.append(...floorsArray);
 }
-
+const floorCalls = new Set();
 document.addEventListener('click', (e) => {
   const floorCall = Number(e.target.dataset.floor);
+  floorCalls.add(floorCall);
   if (floorCall) {
     //Move the lift
     moveLift(floorCall);
   }
 });
 
-function moveLift(floorCall) {
-  const lifts = document.querySelectorAll('.lift');
-  for (const lift of lifts) {
-    const currentFloor = Number(lift.dataset.floor);
-    const { height } = document
-      .querySelector(`[data-floor='${floorCall}']`)
-      .getBoundingClientRect();
-    if (currentFloor !== floorCall && !lift.classList.contains('busy')) {
-      lift.style.transform = `translateY(-${height * (floorCall - 1)}px)`;
-      lift.style.transition = `all ${
-        Math.abs(floorCall - currentFloor) * 2
-      }s ease-in`;
-      lift.dataset.floor = floorCall;
-      lift.classList.add('busy');
+function getClosesLift(floorCall, lifts) {
+  let distance = 0;
+  let lift = '';
 
-      lift.addEventListener('transitionend', () =>
-        lift.classList.remove('busy')
-      );
+  for (const _lift of lifts) {
+    // getting distance between the currentFloorCall and the lift
+    const floorDistance = Math.abs(floorCall - Number(_lift.dataset.floor));
 
-      break;
+    // finding closest lift to the floorCall based on floor Distance
+    if (distance > floorDistance || !distance) {
+      distance = floorDistance;
+      lift = _lift;
     }
+  }
+
+  return { lift, distance };
+}
+
+function moveLift(floorCall) {
+  const lifts = Array.from(document.querySelectorAll('.lift'));
+
+  // getting all the non-busy lifts
+  const nonBusyLifts = lifts.filter((lift) => !lift.classList.contains('busy'));
+
+  // get the closes lift to the current Floor
+  const { lift, distance } = getClosesLift(floorCall, nonBusyLifts);
+
+  const currentFloor = Number(lift.dataset.floor);
+
+  const { height } = document
+    .querySelector(`[data-floor='${floorCall}']`)
+    .getBoundingClientRect();
+
+  if (currentFloor !== floorCall) {
+    lift.style.transform = `translateY(-${height * (floorCall - 1)}px)`;
+    lift.style.transition = `all ${distance * 2}s ease-in`;
+    lift.dataset.floor = floorCall;
+    lift.classList.add('busy');
+
+    lift.addEventListener('transitionend', () => lift.classList.remove('busy'));
   }
 }
