e226e5bd61c499633ca216a78b0f38f92a4f13e5
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-09-27 11:48:06 +0530

    fix: form validations

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
-const generateBtn = document.getElementById('generate-btn');
-const floorsInput = document.getElementById('floors-input');
-const liftsInput = document.getElementById('lifts-input');
-const floorsGroup = document.getElementById('floors-group');
+const form = document.querySelector(".form");
+const floorsGroup = document.getElementById("floors-group");
 
-generateBtn.addEventListener('click', () => {
-  const floorsCount = Number(floorsInput.value);
-  const liftsCount = Number(liftsInput.value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target.elements);
+  const floorsCount = Number(e.target.elements[0].value);
+  const liftsCount = Number(e.target.elements[1].value);
 
   const screenWidth = screen.availWidth;
 
   if (screenWidth < 600 && liftsCount > 2) {
-    alert('This screen size cant have more than 2 lifts');
+    alert("This screen size cant have more than 2 lifts");
   } else {
-    floorsGroup.innerHTML = '';
+    floorsGroup.innerHTML = "";
     buildSimulation(floorsCount, liftsCount);
   }
 });
@@ -20,34 +20,34 @@ generateBtn.addEventListener('click', () => {
 function buildSimulation(floors, lifts) {
   const floorsArray = [];
 
-  const liftsGroup = document.createElement('div');
-  liftsGroup.setAttribute('class', 'lifts');
+  const liftsGroup = document.createElement("div");
+  liftsGroup.setAttribute("class", "lifts");
 
   // generate Lifts
   for (let i = 0; i < lifts; i++) {
-    const lift = document.createElement('div');
-    lift.setAttribute('class', 'lift');
-    lift.setAttribute('data-floor', `1`);
-    lift.setAttribute('data-lift', `${i + 1}`);
+    const lift = document.createElement("div");
+    lift.setAttribute("class", "lift");
+    lift.setAttribute("data-floor", `1`);
+    lift.setAttribute("data-lift", `${i + 1}`);
     liftsGroup.append(lift);
   }
 
   // generate floors
   for (let i = 0; i < floors; i++) {
-    const floor = document.createElement('div');
-    floor.setAttribute('class', 'floor');
-    floor.setAttribute('data-floor', `${i + 1}`);
-    const liftButtons = document.createElement('div');
-    liftButtons.setAttribute('class', 'lift-buttons');
-    const callBtn = document.createElement('button');
-    callBtn.setAttribute('class', `floor-call-btn`);
-    callBtn.setAttribute('data-floor', `${i + 1}`);
-    callBtn.textContent = 'Call';
-    const floorTitle = document.createElement('h3');
+    const floor = document.createElement("div");
+    floor.setAttribute("class", "floor");
+    floor.setAttribute("data-floor", `${i + 1}`);
+    const liftButtons = document.createElement("div");
+    liftButtons.setAttribute("class", "lift-buttons");
+    const callBtn = document.createElement("button");
+    callBtn.setAttribute("class", `floor-call-btn`);
+    callBtn.setAttribute("data-floor", `${i + 1}`);
+    callBtn.textContent = "Call";
+    const floorTitle = document.createElement("h3");
     floorTitle.textContent = `Floor ${i + 1}`;
     liftButtons.append(callBtn, floorTitle);
-    const floorContainer = document.createElement('div');
-    floorContainer.setAttribute('class', 'floor-container');
+    const floorContainer = document.createElement("div");
+    floorContainer.setAttribute("class", "floor-container");
     floorContainer.append(liftButtons);
     if (i === 0) floorContainer.append(liftsGroup);
 
@@ -60,7 +60,7 @@ function buildSimulation(floors, lifts) {
 
 let previousCall;
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
   const floorCall = Number(e.target.dataset.floor);
 
   if (floorCall && previousCall !== floorCall) {
@@ -71,10 +71,10 @@ document.addEventListener('click', (e) => {
 });
 
 function processRequest(floorCall) {
-  const lifts = Array.from(document.querySelectorAll('.lift'));
+  const lifts = Array.from(document.querySelectorAll(".lift"));
 
   // getting all the non-busy lifts
-  const nonBusyLifts = lifts.filter((lift) => !lift.classList.contains('busy'));
+  const nonBusyLifts = lifts.filter((lift) => !lift.classList.contains("busy"));
 
   if (nonBusyLifts.length) {
     // get the closes lift to the current Floor
@@ -89,7 +89,7 @@ function processRequest(floorCall) {
 
 function getClosestLift(floorCall, lifts) {
   let distance = null;
-  let lift = '';
+  let lift = "";
 
   for (const _lift of lifts) {
     // getting distance between the currentFloorCall and the lift
@@ -115,27 +115,27 @@ function moveLift(floorCall, lift, distance) {
     lift.style.transform = `translateY(-${height * (floorCall - 1)}px)`;
     lift.style.transition = `all ${distance * 2}s ease-in`;
     lift.dataset.floor = floorCall;
-    lift.classList.add('busy');
+    lift.classList.add("busy");
 
     // open doors when lift has reached the floor
     setTimeout(() => {
-      lift.classList.add('door-operation');
+      lift.classList.add("door-operation");
     }, distance * 2000 + 500);
 
     // free the lift
-    lift.addEventListener('transitionend', (e) => {
+    lift.addEventListener("transitionend", (e) => {
       setTimeout(() => {
-        lift.classList.remove('door-operation');
-        lift.classList.remove('busy');
+        lift.classList.remove("door-operation");
+        lift.classList.remove("busy");
       }, 5500);
     });
   } else {
     // If lift at same floor open doors
-    lift.classList.add('door-operation');
-    lift.classList.add('busy');
+    lift.classList.add("door-operation");
+    lift.classList.add("busy");
     setTimeout(() => {
-      lift.classList.remove('door-operation');
-      lift.classList.remove('busy');
+      lift.classList.remove("door-operation");
+      lift.classList.remove("busy");
     }, 5500);
   }
 }
